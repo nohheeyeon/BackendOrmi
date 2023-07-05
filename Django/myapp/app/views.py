3a380121db02091b45a85ba430b4dbf24d9daa48
nohheeyeon <130336617+nohheeyeon@users.noreply.github.com>
2023-07-06 00:33:07 +0900

    Delete Django directory

--- Django/myapp/app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from django.views import View
-from django.urls import reverse_lazy, reverse
-
-
-class IndexMain(View):
-    def get(self, request):
-        return render(request, 'index.html')
